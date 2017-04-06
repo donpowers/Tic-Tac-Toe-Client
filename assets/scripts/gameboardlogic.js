e7daf10b1f0f84681c5f8147b1920ebcb80ed4ff
dpowers59 <don.powers@yahoo.com>
2017-04-06 11:42:53 -0400

    Game board and logic added
    Game board html was updated to support the game logic.
    
    Functionality added:
      Add Event Handlers
      On click perform:
        Determine which player created the move
        Update the game board score
        Update the image to reflect play
        Check for a winner
        Check for any moves left

--- assets/scripts/gameboardlogic.js
+++ assets/scripts/gameboardlogic.js
@@ -25,49 +25,138 @@ const gameCellIDs = [
   'cell-7',
   'cell-8'
 ]
-
+const currentGame = {
+  'game': {
+    'id': 1,
+    'cells': ['', '', '', '', '', '', '', '', ''],
+    'over': true,
+    'player_x': {
+      'id': 1,
+      'email': 'and@and.com'
+    },
+    'player_o': {
+      'id': 3,
+      'email': 'dna@dna.com'
+    }
+  }
+}
+const winningCombinations = [
+  ['0', '1', '2'],
+  ['3', '4', '5'],
+  ['6', '7', '8'],
+  ['0', '3', '6'],
+  ['1', '4', '7'],
+  ['2', '5', '8'],
+  ['6', '4', '2'],
+  ['0', '4', '8']
+]
 const flipMark = function () {
   console.log('flipMark Called', this)
   console.log('gameBoardImages[0].markImage: ' + gameBoardImages[1].markImage)
-  this.setAttribute('src', gameBoardImages[1].markImage)
+  // update move
+  if (isXplaying()) {
+    currentGame.game.cells[parseInt(transformLocation(this.id))] = 'x'
+  } else {
+    currentGame.game.cells[parseInt(transformLocation(this.id))] = 'o'
+  }
+  console.log('currentGame: ', currentGame)
+  if (isXplaying()) {
+    this.setAttribute('src', gameBoardImages[0].markImage)
+  } else {
+    this.setAttribute('src', gameBoardImages[1].markImage)
+  }
   // disable click
+  console.log('ID: ' + this.id)
+  $(this.id).off('click')
+  // check for a winner
+  if (checkForWinner()) {
+    alert('We have a winner')
+    // update who is the next player
+    updatePlayerButton()
+  } else if (anyMovesLeft()) {
+    updatePlayerButton()
+  } else {
+    alert('Cats Meow, Try Again')
+  }
 }
 
 // Adds the cards to the DOM.
 const setUpGameBoard = function () {
   console.log('setUpGameBoard')
   for (let i = 0; i < gameCellIDs.length; i++) {
-    const element = document.getElementById(gameCellIDs[i] + '-img')
-    element.addEventListener('click', flipMark)
-    // const newImgElement = document.createElement('img')
-    // newImgElement.setAttribute('src', gameBoardImages[0].image)
-    // newImgElement.setAttribute('img_id', i)
-    // element.appendChild(newImgElement)
+    const elementID = gameCellIDs[i] + '-img'
+    console.log('elementID: ' + elementID)
+    console.log($('elementID'))
+    $('elementID').one('click', flipMark)
+    // c onst element = document.getElementById(elementID)
+    // element.addEventListener('click', flipMark)
   }
+}
+const updatePlayerButton = function () {
+  const element = document.getElementById('game-button')
+  console.log('updatePlayer called')
+  if (isXplaying()) {
+    element.value = 'It\'s O\'s Turn!'
+  } else {
+    element.value = 'It\'s X\'s Turn!'
+  }
+}
+const isXplaying = function () {
+  const element = document.getElementById('game-button')
+  console.log('updatePlayer called')
+  let result = true
+  if (!(element.value === 'It\'s X\'s Turn!')) {
+    console.log('isXplay no: ' + element.value)
+    result = false
+  }
+  console.log('isXplaying returning', result)
+  return result
+}
+
+const transformLocation = function (imgID) {
+  const data = imgID.split('-')
+  console.log('transformLocation returning:' + data[1])
+  return data[1]
 }
 
 const replayButtonClick = function () {
   console.log('replayButtonClick')
-// Clear cards in play
-
-// reset images...since cardsInPlay are node elements could just reset those.
-// get a list of all the img nodes and update the image displayed.
-// var newListItem = document.getElementsByTagName('img');
-// for( var i=0; i < newListItem.length; i++ ) {
-// newListItem[i].setAttribute('src','images/back.png');
-// }
 }
-// Why didn't this work?
-// const addReplayButtonListner = function() {
-// document.getElementById('replayBtn').addEventListner('click', replayButtonClick);
-// }
 
-const checkForMatch = function () {
-  const result = 0
+const checkForWinner = function () {
+  let result
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const value1 = currentGame.game.cells[winningCombinations[i][0]]
+    const value2 = currentGame.game.cells[winningCombinations[i][1]]
+    const value3 = currentGame.game.cells[winningCombinations[i][2]]
+    console.log('game moves: ' + currentGame.game.cells)
+    console.log(winningCombinations[i][0] + winningCombinations[i][1] + winningCombinations[i][2])
+    console.log('value1: ' + value1 + ' value2: ' + value2 + ' value3: ' + value3)
+    if (value1 === '' || value2 === '' || value3 === '') {
+      continue
+    }
+    if (value1 === value2 && value1 === value3) {
+      result = value1
+      break
+    }
+  }
+  console.log('checkForWinner returning: ' + result)
   return result
 }
+const anyMovesLeft = function () {
+  let result = false
+  for (let i = 0; i < winningCombinations.length; i++) {
+    if (currentGame.game.cells[i].length > 0) {
+      continue
+    } else {
+      result = true
+      break
+    }
+  }
+  console.log('anyMovesLeft: ' + result)
+  return result
+}
+
 module.exports = {
-  checkForMatch,
-  replayButtonClick,
   setUpGameBoard
 }
